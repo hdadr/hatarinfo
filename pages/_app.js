470f8146786d546c45f52fa5888c762c014563b9
name <hegedis.andor@gmail.com>
2021-07-31 15:51:01 +0200

    add firebase to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../store";
+import { initializeFirebase } from "../firebase";
 
 function BorderInfoApp({ Component, pageProps }) {
+  initializeFirebase();
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
