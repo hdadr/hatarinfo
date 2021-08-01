e6e75b858633dbee37bfc3b79012deb96ceabddc
name <hegedis.andor@gmail.com>
2021-08-01 13:29:38 +0200

    simplify firestore utils function

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,15 +1,9 @@
 import { addDoc, collection, getDocs, getFirestore, query } from "firebase/firestore";
-import { Observable } from "rxjs";
+import { from, Observable } from "rxjs";
 
 export const setDocument = (collectionPath, data) => {
   const db = getFirestore();
-
-  return new Observable((observer) => {
-    addDoc(collection(db, collectionPath), data)
-      .then((docRef) => observer.next(docRef))
-      .catch((error) => observer.error(error))
-      .finally(() => observer.complete());
-  });
+  return from(addDoc(collection(db, collectionPath), data));
 };
 
 export const getDocuments = (collectionPath, constraints) => {
@@ -17,13 +11,5 @@ export const getDocuments = (collectionPath, constraints) => {
 
   const dbCollection = collection(db, collectionPath);
   const q = constraints ? query(dbCollection, constraints) : dbCollection;
-
-  return new Observable((observer) => {
-    getDocs(q)
-      .then((snapshot) => {
-        observer.next(snapshot.docs.map((doc) => doc.data()));
-      })
-      .catch((error) => observer.error(error))
-      .finally(() => observer.complete());
-  });
+  return from(getDocs(q));
 };
