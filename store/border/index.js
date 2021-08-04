23a6d99351214e5ac49215ff4e31ceb62b37bb52
name <hegedis.andor@gmail.com>
2021-08-04 13:19:28 +0200

    add ability to swap border

--- store/border/index.js
+++ store/border/index.js
@@ -1,9 +1,14 @@
-import { SET_SELECTED_BORDER } from "./actions";
+import { SET_SELECTED_BORDER, SWAP_SELECTED_BORDER } from "./actions";
+import { borders } from "./borders";
 
 export const selectedBorder = (state = null, action) => {
   switch (action.type) {
     case SET_SELECTED_BORDER:
       return action.payload;
+
+    case SWAP_SELECTED_BORDER:
+      return borders.find((border) => border.name === action.payload);
+
     default:
       return state;
   }
