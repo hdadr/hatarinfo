693b7a2064711deec1a17e8567529c85e1e12979
name <hegedis.andor@gmail.com>
2021-08-11 13:43:38 +0200

    change typography of border names in borderlist

--- components/BorderList/index.js
+++ components/BorderList/index.js
@@ -1,4 +1,4 @@
-import { Box, Divider, Grow, List, ListItem } from "@material-ui/core";
+import { Box, Divider, Grow, List, ListItem, Typography } from "@material-ui/core";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setSelectedBorder } from "../../store/border/actions";
@@ -26,8 +26,13 @@ const BorderList = ({ borders = [], openBorderInformation }) => {
             <Grow in={true}>
               <ListItem disabled={borderClosed(border.openingHours)} onClick={() => handleBorderListItemClick(border)} button>
                 <div>
-                  <div>{border.name}</div>
-                  <Box color="text.secondary">{border.openingHours}</Box>
+                  <div>
+                    <Typography display="inline">{border.name.split("-")[0]} </Typography>
+                    <Typography display="inline" color="textSecondary">
+                      -{border.name.split("-")[1]}
+                    </Typography>
+                  </div>
+                  <Box color="text.secondary">{border.openingHours}h</Box>
                 </div>
               </ListItem>
             </Grow>
